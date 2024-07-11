157fe84ac542231af2cb1a8503c25e759d0db4ad
kevintsai <kevintsai@TD-ID10-01460.taidoc.com.tw>
2024-07-11 10:03:26 +0800

    first release

---
+++ src/main/java/com/example/ai/SpringBootAI1Application.java
@@ -0,0 +1,13 @@
+package com.example.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAI1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAI1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ai/SpringBootAI1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAI1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
